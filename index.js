ce7db05961810e09cc27cba9a7b8adde17b223d5
Roshanak Akbarifar <roshanak.akbarifar@gmail.com>
2021-06-25 22:26:20 +0000

    index.js modified to contain sum of array

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let total = 0;
+  array.forEach((item) => {
+    if (Array.isArray(item)) total += sumItems(item);
+    else total += item;
+  });
+  return total;
 }
 
+//console.log(sumItems([1, 5, 10]));
 module.exports = sumItems;
